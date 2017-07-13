394cd34ab91a6c7f3db0ecc78d60bd426097fbe3
coder board <1019463979@qq.com>
2017-07-13 16:18:44 +0800

    douban movies

---
+++ pages/movie/movie.js
@@ -0,0 +1,22 @@
+var API_URL = "https://api.douban.com/v2/movie/subject/";
+Page({
+	data:{
+       movie:{}
+	},
+	onLoad:function(params){
+		var that = this;
+       wx.request({
+          url:API_URL+params.id,
+          data:{},
+          header:{
+          	'content-type':'json'
+          },
+          success:function(res){
+          	that.setData({
+          		movie:res.data
+          	});
+          }
+
+       });
+	},
+})
---
+++ pages/search/search.js
@@ -0,0 +1,29 @@
+var API_URL = 'https://api.douban.com/v2/movie/search';
+Page({
+	data:{
+        movies:[]
+	},
+	search:function(e){
+		if(!e.detail.value){
+              return;
+		}
+		wx.showToast({
+			title:"加载中...",
+			icon:"loading",
+			duration:10000
+		});
+		var that = this;
+		wx.request({
+			url:API_URL+"?q="+e.detail.value,
+			data:{},
+			header:{'content-type':'json'
+		    },
+		    success:function(res){
+                wx.hideToast();
+                that.setData({
+                	movies:res.data.subjects
+                });
+		    },
+		});
+	}
+})
